7f4a330c18a6a83008e919db5684976d1eff479d
AlveinTeng <prodigytmx@gmail.com>
2021-08-04 23:57:40 +0800

    Top comment

--- milestone 1/obstacles.asm
+++ milestone 1/obstacles.asm
@@ -1,4 +1,35 @@
-# obstacles
+ CSC258 Summer 2021 Assembly Final Project
+# University of Toronto
+#
+# Student: Mingxuan Teng, 1004070952, tengmin1
+
+#	  Huifeng Wu, 1003802753
+# Bitmap Display Configuration:
+# - Unit width in pixels: 8 (update this as needed)
+# - Unit height in pixels: 8 (update this as needed)
+# - Display width in pixels: 256 (update this as needed)
+# - Display height in pixels: 256 (update this as needed)
+# - Base Address for Display: 0x10008000 ($gp)
+#
+# Which milestones have been reached in this submission? Milestone 1
+# (See the assignment handout for descriptions of the milestones)
+# - Milestone 1/2/3 (choose the one that applies)
+#
+# Which approved features have been implemented for milestone 3?
+# (See the assignment handout for the list of additional features)
+# 1. (fill in the feature, if any)
+# 2. (fill in the feature, if any)
+# 3. (fill in the feature, if any)
+# ... (add more if necessary)
+#
+# Link to video demonstration for final submission:
+# - (insert YouTube / MyMedia / other URL here). Make sure we can view it!
+#
+# Are you OK with us sharing the video with people outside course staff?
+# - yes / no / yes, and please share this project github link as well!
+#
+# Any additional information that the TA needs to know:
+# - (write here, if any)
 
 .data
     displayAddress: 	.word 0x10008000
